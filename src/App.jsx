b44a6fb0951db3e93fa40784272b52aa537f238c
Alex_Bellamy <alexlbellamy@gmail.com>
2020-08-02 23:55:36 +0200

    adding handleSubmit to form and function accordingly to fetch pokemon by gender from api, saving response in json format

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,37 @@
 import React, { Component } from "react";
 
 class App extends Component {
-state = {
-  searchTerm: [],
-}
+  state = {
+    searchTerm: [],
+  };
 
-onChange = (e) => {
-  this.setState({ searchTerm: e.target.value });
-  }
+  onChange = (e) => {
+    this.setState({ searchTerm: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const url = `https://pokeapi.co/api/v2/gender/${this.state.searchTerm}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) =>
+        this.setState({
+          pokemon: json,
+        })
+      );
+  };
 
   render() {
     return (
       <div>
         <h1 id="header">Filter Pokemon by Gender</h1>
         <h2 id="sub-header">Which Pokemon will win the battle?</h2>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input
             id="search"
-            placeholder="genderless, male or female" value={this.state.searchTerm} onChange={this.onChange}
+            placeholder="genderless, male or female"
+            value={this.state.searchTerm}
+            onChange={this.onChange}
           />
           <button id="find">Find</button>
         </form>
