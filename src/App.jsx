48644c90eb0e1380ecd1a827bac4a5f90a07f06f
Alex_Bellamy <alexlbellamy@gmail.com>
2020-08-02 23:24:22 +0200

    adding basic html structure including title and subtitle, user input field and find button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 id="header">Filter Pokemon by Gender</h1>
+        <h2 id="sub-header">Which Pokemon will win the battle?</h2>
+        <form>
+          <input
+            id="search"
+            placeholder="genderless, male or female"
+          />
+          <button id="find">Find</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
